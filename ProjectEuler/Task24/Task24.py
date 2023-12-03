4f83fa9735f174ec4367bf981dbdf51b05a5b00c
yesman2000s <105079628+yesman2000s@users.noreply.github.com>
2023-12-03 19:42:30 +0000

    Heaps algorith working!

--- ProjectEuler/Task24/Task24.py
+++ ProjectEuler/Task24/Task24.py
@@ -4,21 +4,26 @@ numbers = [0,1,2,3,4,5,6,7,8,9]
 def isEven(Number):
     return (Number%2) == 0
 
+foundPermutations = []
 
-def generatePermutations(numbersList , length):
-    if length == 1:
-        print(numbersList)
-    else:
-        # Generate permutations with k-th unaltered
-        # Initially k = length(A)
-        generatePermutations(numbersList , length - 1)
+def generatePermutations(numbersList, length):
+        if length == 1:
+            # A permutation was found!!
+            print(numbersList)
+            foundPermutations.append(numbersList.copy())
+        else:
+            # Generate permutations with k-th unaltered
+            # Initially k = length(A)
+            generatePermutations(numbersList, length - 1)
 
-        for x in range(1,length):
-            if isEven(length):
-                numbersList[x] , numbersList[length-1] = numbersList[length-1] , numbersList[x] 
-            else:
-                numbersList[0] , numbersList[length-1] = numbersList[length-1] , numbersList[0] 
-        generatePermutations(numbersList,length-1)
-        
-        print(numbersList)
-generatePermutations([1,2,3,4] , 2)
+            for x in range(length - 1):
+                if isEven(length):
+                    # Swap elements at x and length-1
+                    numbersList[x], numbersList[length-1] = numbersList[length-1], numbersList[x]
+                else:
+                    # Swap elements at 0 and length-1
+                    numbersList[0], numbersList[length-1] = numbersList[length-1], numbersList[0]
+                generatePermutations(numbersList, length - 1)
+
+generatePermutations([0,1,2],3)
+print(foundPermutations)
