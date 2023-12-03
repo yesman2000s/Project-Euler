1e13f3a288620b06fc0afe78c7907aaa71c92c1b
yesman2000s <105079628+yesman2000s@users.noreply.github.com>
2023-12-03 20:01:08 +0000

    Task 26 start

---
+++ ProjectEuler/Task26/Task26.py
@@ -0,0 +1,14 @@
+def longestRepeatedChain(number:str):
+    
+    
+
+
+lengthOfLongest = 0
+
+for d in range(1,1000):
+    decimal = 1 / d
+    decimal = str(decimal)
+
+    decimal = decimal.split(".")[1]
+
+    print(decimal)
